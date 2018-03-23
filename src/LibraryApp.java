9c2e784f4225c5e93e5cbc3cd23bb1afbbbcf84a
Nathan-Wen <36411251+Nathan-Win-Wen@users.noreply.github.com>
2018-03-23 09:45:03 -0400

    Update LibraryApp.java

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -2,6 +2,15 @@ public class LibraryApp {
     private Library library = new Library();
 
     public static void main(String[] args) {
-        System.out.println(library);
+        for(int i = 0;i < Library.members.length;i ++) {
+            System.out.println("member " + i + " : " + Library.members[i]);
+        }
+    
+        for(int i = 0;i < Libray.books.length;i ++) {
+            System.out.println("book " + i + " : " + Libray.books[i]
+        }   
+                               
+        System.out.println(library.location);
     }
+    
 }
