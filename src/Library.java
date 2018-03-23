57092e1c41c73e644a7b194027dfac7c61961fdf
Nathan-Wen <36411251+Nathan-Win-Wen@users.noreply.github.com>
2018-03-23 09:30:29 -0400

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,39 +1,43 @@
-/**
- * Library class
- */
 public class Library {
+    String name;
+    Address location;
+    Book[] books;
+    Member[] members;
 
-
-    /**
-     * addBook(Book) : void
-     * Creates a new array which is a copy of this.books, but, whose length is
+     /**
+      * addBook(Book) : void
+      *creates a new array which is a copy of this.books, but, whose length is
      * this.books.length + 1, to be able to add the new book.
      *
      * this.books is then replaced by the new array.
-     *
-     * @param book
-     *          A new book object to add to the books list
-     */
+      * @param book
+      *          A new book object to add to the books list
+      */
 
 
-
-
-
-
-    /**
-     * addMember(Member) : void
-     * Creates a new array which is a copy of this.members, but, whose length is
-     * this.members.length + 1, to be able to add the new member.
-     *
-     * this.members is then replaced by the new array.
-     *
-     * @param member
-     *          A new member object to add to the members list
-     */
-
-
-
-
-
-
-}
+       
+       public void addBook (book) {
+           String[] newBooks = new String[this.books.length + 1]
+           for (int i = 0; i < this.books.length; i ++) {
+            newBooks[i] = this.books[i];
+           }
+           
+           newBooks[i] = book;
+           
+       }
+ 
+ 
+     /* @param member
+      *          A new member object to add to the members list
+      */
+     public void addMember (member) {
+           String[] newMember = new String[this.members.length + 1]
+           for (int i = 0; i < this.members.length; i ++) {
+            newMember[i] = this.members[i];
+           }
+      
+           newMember[i] = member;
+           
+ }
+ 
+ 
